12017d206e9de0070a0f99cd2e7a982dfd83e0bc
Johannes Maier <johannes.maier@mailbox.org>
2023-05-29 20:03:04 +0200

    Show some SDL2 window

--- src-exe/Main.hs
+++ src-exe/Main.hs
@@ -1,4 +1,47 @@
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import Control.Monad
+import Data.Function ((&))
+import SDL (($=))
+import SDL qualified
+import SDL.Vect
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  SDL.initialize [SDL.InitVideo]
+  win <- SDL.createWindow "foo" SDL.defaultWindow {SDL.windowInitialSize = V2 640 480}
+  SDL.showWindow win
+  renderer <-
+    SDL.createRenderer
+      win
+      (-1)
+      SDL.RendererConfig
+        { SDL.rendererType = SDL.AcceleratedRenderer,
+          SDL.rendererTargetTexture = False
+        }
+  SDL.rendererDrawColor renderer $= V4 83 50 maxBound maxBound
+  let loop = do
+        evts <- fmap SDL.eventPayload <$> SDL.pollEvents
+        let quit =
+              evts
+                & fmap
+                  ( \case
+                      SDL.KeyboardEvent e | SDL.keyboardEventKeyMotion e == SDL.Pressed ->
+                        case SDL.keysymKeycode (SDL.keyboardEventKeysym e) of
+                          SDL.KeycodeEscape -> True
+                          _ -> False
+                      SDL.QuitEvent -> True
+                      _ -> False
+                  )
+                & or
+        SDL.clear renderer
+        SDL.present renderer
+        unless quit loop
+  loop
+  SDL.destroyRenderer renderer
+  SDL.destroyWindow win
+  SDL.quit
