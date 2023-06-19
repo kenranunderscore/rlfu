f415a14e91b8c5d55253f58f6b5cf166e0c5850d
Johannes Maier <johannes.maier@mailbox.org>
2023-06-19 17:27:53 +0200

    Calculate FPS

--- src-exe/Main.hs
+++ src-exe/Main.hs
@@ -7,7 +7,7 @@ module Main where
 
 import Foreign.C.Types (CInt)
 import ECS
-import Data.Word (Word8)
+import Data.Word (Word8, Word32)
 import Control.Monad
 import Data.Function ((&))
 import SDL (($=))
@@ -23,13 +23,27 @@ data Renderable = Renderable
   }
   deriving stock (Show)
 
+data Components = Components
+  { renderables :: ComponentArray Renderable
+  }
+  deriving stock (Show)
+
+calcFpsInfo :: (Word32, Int) -> IO (Word32, Int)
+calcFpsInfo (t, nframes) = do
+  ticks <- SDL.ticks
+  if t < ticks - 250
+    then do
+      when (ticks > 250) $ putStrLn $ "FPS: " <> show (4 * nframes)
+      pure (ticks, 0)
+    else do
+      pure (t, nframes + 1)
+
 main :: IO ()
 main = do
   let e = fst $ createEntity allEntities
-      renderables = addComponent e (Renderable 100 200 (V4 100 maxBound 20 maxBound) 30) emptyComponentArray
-  print renderables
+      allComponents = Components $ addComponent e (Renderable 100 200 (V4 100 maxBound 20 maxBound) 30) emptyComponentArray
   Sdl.withSdlEnvironment $ \_win renderer -> do
-    let loop = do
+    let loop comps frameInfo = do
           SDL.rendererDrawColor renderer $= V4 83 50 maxBound maxBound
           SDL.clear renderer
 
@@ -38,8 +52,8 @@ main = do
                 evts
                   & fmap
                     ( \case
-                        SDL.KeyboardEvent e | SDL.keyboardEventKeyMotion e == SDL.Pressed ->
-                          case SDL.keysymKeycode (SDL.keyboardEventKeysym e) of
+                        SDL.KeyboardEvent evt | SDL.keyboardEventKeyMotion evt == SDL.Pressed ->
+                          case SDL.keysymKeycode (SDL.keyboardEventKeysym evt) of
                             SDL.KeycodeEscape -> True
                             _ -> False
                         SDL.QuitEvent -> True
@@ -47,10 +61,14 @@ main = do
                     )
                   & or
 
-          forM_ (components renderables) $ \r -> do
+          forM_ (components $ renderables comps) $ \r -> do
             SDL.rendererDrawColor renderer $= color r
             SDL.fillRect renderer $ Just $ SDL.Rectangle (P $ V2 (x r) (y r)) (V2 (sideLength r) (sideLength r))
 
           SDL.present renderer
-          unless quit loop
-    loop
+          let !newRenderables = fmap (\r -> r{x = x r + 1}) (components $ renderables comps)
+              !newComps = comps{renderables = (renderables comps){components = newRenderables}}
+          (newTime, frameCounter') <- calcFpsInfo frameInfo
+          unless quit (loop newComps (newTime, frameCounter'))
+    now <- SDL.ticks
+    loop allComponents (now, 0 :: Int)
